a92412539ea29423ae747b40aa9e75a294d24d17
foobar <none@nowhere.no>
2016-08-17 11:48:30 +0200

    add a skript to view differences between Frankziskas versions and those here

---
+++ franziska.sh
@@ -0,0 +1,14 @@
+dir=$(pwd)
+cd  ../hochrechnungen
+git checkout devel
+cd $dir
+tfa=/tmp/franzi
+tfb=/tmp/local
+ls -1 ../hochrechnungen/HR_Funktionen/BWI3/R/ > $tfa
+ls -1 R > $tfb
+sdiff $tfa $tfb
+file=${1:-R/bits_and_pieces.R}
+ffile=../hochrechnungen/HR_Funktionen/BWI3/$file
+file $ffile
+diffuse $file $ffile
+
