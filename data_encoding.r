69676ab651f971d6660e09849de143ffb4d4e833
foobar <none@nowhere.no>
2016-08-18 13:47:09 +0200

    get the automated data Encoding() issue right.
    got rid of lengthiest of the 3 remaining warnings, leaving only 2 more.
    
    - data_encoding.r: dito.
    - data/*RData: the result of above.
    - dev_check.Rout: the result of above.
    - DESCRIPTION: increase patch level.

--- data_encoding.r
+++ data_encoding.r
@@ -1,30 +1,24 @@
-load("data/bwi.RData")
-
-if (TRUE) {
-    # manual try
-    Encoding(levels(baeume.1$Bemerk)) <- "UTF-8"
-    Encoding(levels(baeume.2$Bemerk)) <- "UTF-8"
-    Encoding(levels(baeume.3$Bemerk)) <- "UTF-8"
-    Encoding(levels(totholz.2$Bemerkung)) <- "UTF-8"
-    Encoding(levels(totholz.3$Bemerkung)) <- "UTF-8"
-} else {
-    for (object in ls()) {
-        obj <- get(object)
-        if (is.data.frame(obj)) {
-            factor_columns <- names(obj[which(sapply(obj, is.factor))])
-            for (factor_column in factor_columns) {
-                levels(obj[[factor_column]]) <- sub("^ ", "", levels(obj[[factor_column]]))
-                Encoding(levels(obj[[factor_column]])) <- "UTF-8"
-                Encoding(levels(obj[[factor_column]])) 
+#!/usr/bin/Rscript --vanilla
+.RDatas <- list.files("data", full.names = TRUE)
+for (.RData in .RDatas) {
+    load(.RData)
+    .objects <- ls()
+    for (.object in .objects) {
+        .obj <- get(.object)
+        if (is.list(.obj)) {
+            Encoding(names(.obj)) <- "UTF-8"
+            .factor_columns <- names(.obj[which(sapply(.obj, is.factor))])
+            for (.factor_column in .factor_columns) {
+                levels(.obj[[.factor_column]]) <- sub("^ ", "", levels(.obj[[.factor_column]]))
+                Encoding(levels(.obj[[.factor_column]])) <- "UTF-8"
             }
-            character_columns <- names(obj[which(sapply(obj, is.character))])
-            for (character_column in character_columns) {
-                levels(obj[[character_column]]) <- sub("^ ", "", levels(obj[[character_column]]))
-                Encoding(levels(obj[[character_column]])) <- "UTF-8"
-                Encoding(levels(obj[[character_column]])) 
+            .character_columns <- names(.obj[which(sapply(.obj, is.character))])
+            for (.character_column in .character_columns) {
+                Encoding(.obj[[.character_column]]) <- "UTF-8"
             }
-            assign(object, obj)
+            assign(.object, .obj)
         }
     }
+    save(list = ls(all.names = FALSE), file = .RData)
+    rm(list = ls(all.names = FALSE))
 }
-save.image(file = "data/bwi.RData")
