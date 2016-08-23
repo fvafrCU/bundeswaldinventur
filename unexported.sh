9057eb50c94bbcb7d847debe9fe2eccce33f14ca
foobar <none@nowhere.no>
2016-08-23 11:05:33 +0200

    enhance tool
    don't count anonymous functions starting with a dot argument (probably and
    ellispis.

--- unexported.sh
+++ unexported.sh
@@ -1,6 +1,6 @@
 for file in $( find R/ -type f -print)
 do
-    num_functions=$(grep -F "function(" $file | wc -l)
+    num_functions=$(grep "function([^.]" $file | wc -l)
     num_exported=$(grep -F "#' @export" $file | wc -l)
     echo $num_exported of $num_functions exported in $file.
 done
