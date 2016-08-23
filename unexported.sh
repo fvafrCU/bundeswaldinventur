cfde3fd735304214ed14ffb9279f71c4ae31ebcb
foobar <none@nowhere.no>
2016-08-23 08:23:51 +0200

    make tools more efficient

--- unexported.sh
+++ unexported.sh
@@ -2,5 +2,5 @@ for file in $( find R/ -type f -print)
 do
     num_functions=$(grep -F "function(" $file | wc -l)
     num_exported=$(grep -F "#' @export" $file | wc -l)
-    echo $file $num_exported of $num_functions exported.
+    echo $num_exported of $num_functions exported in $file.
 done
