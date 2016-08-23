1e934d78ab1aba5735020e8234ee8382a5df9656
foobar <none@nowhere.no>
2016-08-23 10:46:59 +0200

    exporting and documenting functions

--- R/bits_and_pieces.R
+++ R/bits_and_pieces.R
@@ -1,4 +1,3 @@
-
 #' add colSums to a data.frame, preserve its row.names
 #'
 #' @author Dominik Cullmann, <dominik.cullmann@@forst.bwl.de>
@@ -7,11 +6,12 @@
 #' @param german If TRUE, the name of the colSums row will be 'Summe' and 'Sum'
 #' otherwise.
 #' @return  the modified data.frame. 
+#' @export
 #' @examples
-#' \dontrun{
-#' add_colSums_to_data_frame(utils::head(airquality), german = F)
-#' }
+#' add_colSums_to_data_frame(utils::head(airquality), german = FALSE)
 add_colSums_to_data_frame <- function(data_frame, german = TRUE) {
+  checkmate::assertDataFrame(data_frame)
+  checkmate::assertLogical(german)
     if (german) sum_label <- 'Summe' else sum_label  <- 'Sum'
     tmp_data <-	rbind(data_frame, sum_label = colSums(data_frame))
     names <- dimnames(data_frame)
@@ -35,6 +35,8 @@ add_colSums_to_data_frame <- function(data_frame, german = TRUE) {
 #' as_percent_and_round(airquality[1:10, "Ozone"] / max(airquality[["Ozone"]],
 #'                      na.rm = TRUE))
 as_percent_and_round <- function(x, digits = 1){
+    checkmate::assertNumeric(x)
+    checkmate::assertInt(digits)
     return(round(x * 100, digits))
 }
 
@@ -56,6 +58,7 @@ as_percent_and_round <- function(x, digits = 1){
 #' set_to_limit(mtcars, mpg < 20)
 #' set_to_limit(set_to_limit(mtcars, mpg < 20), mpg > 23)
 set_to_limit <- function(data_frame, condition) {
+  checkmate::assertDataFrame(data_frame)
     s <- deparse(substitute(condition))
     variable <- strsplit(s, " ")[[1]][1]
     operator <- strsplit(s, " ")[[1]][2]
@@ -65,12 +68,22 @@ set_to_limit <- function(data_frame, condition) {
     eval(parse(text = to_eval))
     return(data_frame)
 }
-# @NULL 
-#FIXME:
+
+#' get period
+#' 
+#' gets the time period for a label.
+#' 
+#' @author Dominik Cullmann, <dominik.cullmann@@forst.bwl.de>
+#' @param label Integer which labels the period (1 or 2).
+#' @export
+#' @return time period (String).
+#' @examples
+#' get_period(2)
 get_period <- function(label) {
+  checkmate::assertNumber(label)
     period <- switch(as.character(label),
                      "1" = "1987 - 2002", 
-                     "2"= "2002 - 2012", 
+                     "2" = "2002 - 2012", 
                      stop("unkown period, give 1 or 2")
                      )
     return(period)
