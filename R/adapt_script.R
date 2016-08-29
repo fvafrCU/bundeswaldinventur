5598599abc650ea2fbd3de017df88dbff846a935
foobar <none@nowhere.no>
2016-08-29 10:45:52 +0200

    make adapt_script() use find_files and clean up the backup/overwrite mess

--- R/adapt_script.R
+++ R/adapt_script.R
@@ -1,27 +1,45 @@
-adapt_script <- function(x, overwrite = FALSE, backup = FALSE) {
-    s <- readLines(x)
-    i <- grep("source\\(.*customization.R[\"\'])", s)
-    dependencies <- c('library("bundeswaldinventur")', 'library("xtable")', 
-                      'library("plyr")', 'library("reshape")', 'library("reshape2")',
-                      'library("ggplot2")', 'library("scales")')
-    header <- s[seq_len(i - 1)]  
-    bottom <- s[seq.int(from = (i + 1), to = length(s), by = 1)]  
+adapt_script <- function(file_names = NA, path = ".", 
+                         pattern = ".*\\.[RrSs]$|.*\\.[RrSs]nw$",
+                         all.files = TRUE, recursive = TRUE, 
+                         ignore.case = FALSE,
+                         overwrite = FALSE, backup = FALSE, verbose = TRUE
+                         ) {
+    file_names <- find_files(file_names = file_names, path = path, 
+                             pattern = pattern, all.files = all.files, 
+                             recursive = recursive, ignore.case = ignore.case)
+    all_text <- NULL
+    for (file_name in file_names) {
+        s <- readLines(file_name)
+        i <- grep("source\\(.*customization.R[\"\'])", s)
+        if (length(i) == 0) {
+            warning("script ", file_name, " doesn't seem to need adaption.")
+        } else {
+            dependencies <- c('library("bundeswaldinventur")', 'library("xtable")', 
+                              'library("plyr")', 'library("reshape")', 'library("reshape2")',
+                              'library("ggplot2")', 'library("scales")')
+            header <- s[seq_len(i - 1)]  
+            bottom <- s[seq.int(from = (i + 1), to = length(s), by = 1)]  
 
-    s <-  c(header, dependencies, bottom)
-    s <- s[ - c(grep("^\\ *provide_data().*", s), 
-                grep("^\\ *provide_statistics().*", s))] 
-    if (backup) {
-        x <- paste0(x, "_adapted")
-        output <- file(x)
-        writeLines(text = s, con = output)
-        close(output)
+            s <-  c(header, dependencies, bottom)
+            s <- s[ - c(grep("^\\ *provide_data().*", s), 
+                        grep("^\\ *provide_statistics().*", s))] 
+            if (isTRUE(overwrite)) {
+                if (backup) {
+                    new_name <- paste0(file_name, "_adapted")
+                    file.copy(file_name, new_name, overwrite = TRUE)
+                    if (isTRUE(verbose)) message("backed up ", file_name, " to ", new_name, ".")
+                }
+                output <- file(file_name)
+                writeLines(text = s, con = output)
+                close(output)
+                    if (isTRUE(verbose)) message("adapted ", file_name, ".")
+            }
+            all_text <- c(all_text, s)
+        }
     }
-    if (overwrite) {
-        output <- file(x)
-        writeLines(text = s, con = output)
-        close(output)
-    }
-    return(s)
+    return(invisible(all_text))
 
 }
-adapt_script("~/git/oudeis/fva/hochrechnungen/scripts/Deines.R", backup = TRUE)
+
+adapt_script(path = "~/git/os/fva/hochrechnungen/scripts/", overwrite = TRUE, backup = TRUE)
+
