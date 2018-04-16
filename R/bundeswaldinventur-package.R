e4543949832906cd02a1d2faa832fd3bb449797f
foobar <none@nowhere.no>
2018-04-16 12:52:15 +0200

    get rid of stale vignette

--- R/bundeswaldinventur-package.R
+++ R/bundeswaldinventur-package.R
@@ -3,9 +3,6 @@
 #' A collection of design-based estimators and reporting functions for
 #' substrata of the area of interest.
 #'
-#' You will find the details in\cr
-#' \code{vignette("An_Introduction_to_bundeswaldinventur", package =
-#' "bundeswaldinventur")}.
 #'
 #' @name bundeswaldinventur-package
 #' @aliases bundeswaldinventur-package
