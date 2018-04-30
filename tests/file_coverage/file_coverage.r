104232744cde28df3e4b175acd41028a761b4193
foobar <none@nowhere.no>
2018-04-30 08:50:08 +0200

    enhance testing

--- tests/file_coverage/file_coverage.r
+++ tests/file_coverage/file_coverage.r
@@ -1,4 +1,4 @@
-name <- "variable_getters.R"
+name <- "options.R"
 root <- rprojroot::find_root(rprojroot::is_r_package)
 source_file <- file.path(root, "R", name)
 test_file <- file.path(root, "tests", "testthat", paste0("test-local_", name))
@@ -6,8 +6,8 @@ test_file <- file.path(root, "tests", "testthat", paste0("test-local_", name))
 if (FALSE) {
     dump("result")
 }
-
 devtools::load_all()
+
 testthat::test_file(test_file)
 cov <- covr::file_coverage(source_file, test_file)
 print(covr::zero_coverage(cov))
